536a1f82dfeb12986cefb666030ae7782d842497
akbarsanihasan <akbarsani.dev@gmail.com>
2025-05-31 08:09:24 +0700

    update: some adjustment

--- distrobox.sh
+++ distrobox.sh
@@ -3,11 +3,11 @@
 clear
 
 if ! command -v docker &>/dev/null; then
-    echo -e "Docker is not installed"
-    exit 1
+	echo -e "Docker is not installed"
+	exit 1
 fi
 
 if ! command -v distrobox &>/dev/null; then
-    curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install |
-        sh -s -- --prefix "$HOME"/.local
+	curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install |
+		sh -s -- --prefix "$HOME"/.local
 fi
